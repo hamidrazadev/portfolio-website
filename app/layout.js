f886ec5bc15724d7987529c2cab92377fcf0ca97
Hamid Raza <itmughal2023@gmail.com>
2025-06-23 01:34:33 +0500

    Implement dark mode support and theme toggle functionality

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers as ThemeProviderUtil } from "@/utils/ThemeProviderUtil";
+import ThemeToggle from "@/components/ThemeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <ThemeProviderUtil>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeToggle />
+          {children}
+        </body>
+      </ThemeProviderUtil>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gray-700 text-white">
+    <div className="bg-gray-700 dark:bg-gray-100 dark:text-black text-white">
       Portfolio Website
     </div>
   );
